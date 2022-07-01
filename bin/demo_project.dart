55f2a658adb26f457908c9ddba267bce372d9d3d
vishnuacappella <acappella.studiov@gmail.com>
2022-07-01 11:15:00 +0530

    read value and sum two numbers

--- bin/demo_project.dart
+++ bin/demo_project.dart
@@ -1,8 +1,20 @@
 import 'dart:io';
 
 void main() {
-  print('Hello World');
+  // print('Hello World');
+  // var a = stdin.readLineSync(); //! input a value
+  // print('Entered value is: $a');
 
-  var a = stdin.readLineSync(); //! input a value
-  print('Entered value is: $a');
+  print('Enter 2 Numbers');
+  var input1 = stdin.readLineSync();
+  var input2 = stdin.readLineSync();
+
+  // !avoid (!) null
+  //! if (input1 == null) {
+  //!   input1 = '0';
+  //! }
+  var num1 = int.parse(input1!);
+  var num2 = int.parse(input2!);
+
+  print('Sum : ${num1 + num2}');
 }
